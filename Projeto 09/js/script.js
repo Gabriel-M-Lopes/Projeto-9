83914e5eaa168b75add4ad477c75ec8c3500372d
Gabriel Macêdo Lopes <gabrielmlopes07@gmail.com>
2025-05-13 17:35:48 -0300

    Add files via upload

---
+++ Projeto 09/js/script.js
@@ -0,0 +1,35 @@
+import { Modal } from './modal.js'
+import { alertError } from './alert-error.js'
+
+const form = document.querySelector('form');
+const inputWeight = document.querySelector('#weight');
+const inputHeight = document.querySelector('#height');
+
+form.onsubmit = function(event) {
+  event.preventDefault();
+
+  const weight = inputWeight.value;
+  const height = inputHeight.value;
+  const showAlertError = notANumber(weight) || notANumber(height);
+
+  if (showAlertError) {
+    alertError.open();
+    return;
+  }
+
+  alertError.close();
+
+  const result = IMC(weight, height);
+  const message = `Seu IMC é de ${result}`;
+
+  Modal.message.innerText = message;
+  Modal.open();
+}
+
+function IMC(weight, height) {
+  return (weight / ((height / 100) ** 2)).toFixed(2);
+}
+
+function notANumber(value) {
+  return isNaN(value) || value == ''
+}
